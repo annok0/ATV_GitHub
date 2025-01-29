32f1e150f7f099bcef9a788a8445a1e4fc4c2510
CaioDalNegro <caiodalnegro@gmail.com>
2025-01-29 15:04:25 -0300

    0.1

---
+++ Ex02-git+POO/Sistemas_Reserva_Hotel/Hospede.java
@@ -0,0 +1,43 @@
+public class Hospede {
+
+    //Atributos
+    public int id;
+    public String nome;
+    public String telefone;
+
+    //Construtor
+    public Hospede() {
+
+    }
+
+    //Métodos
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void ExibirHospede() {
+        return id;
+        return nome;
+        return telefone
+    }
+}
---
+++ Ex02-git+POO/Sistemas_Reserva_Hotel/Reserva.java
@@ -0,0 +1,53 @@
+public class Reserva extends Quarto{
+
+    //Atributos
+    public int id;
+    public Hospede hospede;
+    public Quarto quarto;
+    public int diarias;
+
+    //Construtor
+    public Reserva() {
+    }
+
+    public Reserva(int id, ) {
+        super()
+    }
+
+    //Métodos
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Hospede getHospede() {
+        return hospede;
+    }
+
+    public void setHospede(Hospede hospede) {
+        this.hospede = hospede;
+    }
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+    public void setQuarto(Quarto quarto) {
+        this.quarto = quarto;
+    }
+
+    public int getDiarias() {
+        return diarias;
+    }
+
+    public void setDiarias(int diarias) {
+        this.diarias = diarias;
+    }
+
+    public void CalcularTotal() {
+        return diarias *
+    }
+}
